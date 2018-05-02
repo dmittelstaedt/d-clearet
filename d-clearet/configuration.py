6bfa56a3015ba7844531933e3f47d902dc712ad1
dmittelstaedt <mittelstaedt.david@gmail.com>
2018-05-02 17:28:37 +0200

    Add config file for retention periods

--- d-clearet/configuration.py
+++ d-clearet/configuration.py
@@ -1,1 +1,22 @@
 import configparser
+
+ini_file = "/home/david/Projects/test-share/clearet.ini"
+
+
+def parse():
+    configuration = configparser.ConfigParser()
+    configuration.read(ini_file)
+    return configuration
+
+
+def get_retention_periods(configuration):
+    retention_periods = {}
+    retention = configuration['retention']
+    for key in retention:
+        retention_periods[key] = int(retention[key])
+    return retention_periods
+
+
+def get_data_file(configuration):
+    file = configuration['file']
+    return file['datafile']
--- d-clearet/main.py
+++ d-clearet/main.py
@@ -1,5 +1,6 @@
 import platform
 import file_helper
+import configuration
 from file_info import FileInfo
 
 print(platform.python_version())
@@ -13,39 +14,28 @@ files_read = []
 files_current = []
 file_infos = []
 
-retention_periods = {
-    "w02": 14,
-    "j01": 365,
-    "j02": 730,
-    "j10": 3650,
-    "m01": 30,
-    "m02": 60,
-    "m03": 93,
-    "m04": 120,
-    "m06": 180,
-    "m18": 540,
-    "nol": -1,
-    "od1": -1
-}
+parsed_configuration = configuration.parse()
+print(configuration.get_data_file(parsed_configuration))
+print(configuration.get_retention_periods(parsed_configuration))
 
-if file_helper.check_file_exists(data_file):
-    file_infos = file_helper.load_data(data_file)
-    for file_info in file_infos:
-        files_read.append(file_info.file)
-
-files_current = file_helper.get_all_files(directory, retention_periods)
-
-for file in files_current:
-    if not files_read or file not in files_read:
-        file_infos.append(FileInfo(
-            file,
-            file_helper.get_creation_time(file),
-            file_helper.get_expiration_time(file, retention_periods)))
-
-file_infos_tmp = file_infos[:]
-
-for file_info in file_infos_tmp:
-    if file_helper.remove_file(file_info):
-        file_infos.remove(file_info)
-
-file_helper.save_data(data_file, file_infos)
+# if file_helper.check_file_exists(data_file):
+#     file_infos = file_helper.load_data(data_file)
+#     for file_info in file_infos:
+#         files_read.append(file_info.file)
+#
+# files_current = file_helper.get_all_files(directory, retention_periods)
+#
+# for file in files_current:
+#     if not files_read or file not in files_read:
+#         file_infos.append(FileInfo(
+#             file,
+#             file_helper.get_creation_time(file),
+#             file_helper.get_expiration_time(file, retention_periods)))
+#
+# file_infos_tmp = file_infos[:]
+#
+# for file_info in file_infos_tmp:
+#     if file_helper.remove_file(file_info):
+#         file_infos.remove(file_info)
+#
+# file_helper.save_data(data_file, file_infos)
